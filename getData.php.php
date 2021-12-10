02331df4bbb85934aea101d1f97e4c8e336a5b3a
sokot19 <77233790+sokot19@users.noreply.github.com>
2021-12-10 22:10:02 +0330

    Add files via upload

---
+++ getData.php.php
@@ -0,0 +1,15 @@
+{
+  "setDbKeyValue": {
+    "adsPriority": "adivery,tapsell,magnet,adiveryNative,admob,rayan",
+    "adsPriorityBanner": "adivery,tapsell,magnet,adiveryNative,admob,rayan",
+    "adsPriorityBanner2": "tapsell,adivery,magnet,adiveryNative,admob,rayan",
+    "adsPriorityInterstitial": "adivery,tapsell,magnet,admob",
+    "adsPriorityInterstitialMain": "adivery,tapsell,magnet,admob"
+  },
+  "adstype": "tapsell",
+  "adsPriority": "adivery,tapsell,magnet,adiveryNative,admob,rayan",
+  "adsPriorityBanner": "adivery,tapsell,magnet,adiveryNative,admob,rayan",
+  "adsPriorityBanner2": "tapsell,adivery,magnet,adiveryNative,admob,rayan",
+  "adsPriorityInterstitial": "adivery,tapsell,magnet,admob",
+  "adsPriorityInterstitialMain": "adivery,tapsell,magnet,admob"
+}
